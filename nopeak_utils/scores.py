b045bc710d9b5f854f9544085dacd7aeef076269
Øyvind Almelid <oyvind.almelid@igmm.ed.ac.uk>
2022-10-25 09:39:52 +0100

    clean up scores.py

--- nopeak_utils/scores.py
+++ nopeak_utils/scores.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from typing import List
 
 import pandas as pd
@@ -22,8 +21,6 @@ class ScoreSet:
         self.genome = genome
         sequences = sites.apply(self._get_sequences, axis=1)
         self.sites = sites.join(sequences)
-        self.ref_matches = None
-        self.alt_matches = None
         self.genome_name = genome_name
 
     def _get_sequences(self, row: pd.Series):
@@ -44,59 +41,49 @@ class ScoreSet:
         scanner.set_motifs(self.motifs)
         scanner.set_genome(self.genome_name)
         scanner.set_threshold(fpr=fpr)
-        self.ref_matches = list(scanner.scan(Fasta(fdict=self.sites.ref_seq.to_dict())))
-        self.alt_matches = list(scanner.scan(Fasta(fdict=self.sites.alt_seq.to_dict())))
 
-    def get_best_scores(self, fpr=0.05):
-        """Get the best motif score for each sequence, at a given false positive rate."""
-        self._score_sequences(fpr=fpr)
-        scores = defaultdict(list)
+        ref_matches = list(scanner.scan(Fasta(fdict=self.sites.ref_seq.to_dict())))
+        alt_matches = list(scanner.scan(Fasta(fdict=self.sites.alt_seq.to_dict())))
 
-        for ref_matches, alt_matches, rsid, ref_seq, alt_seq, ar in zip(
-            self.ref_matches,
-            self.alt_matches,
-            self.sites.ID,
-            self.sites.ref_seq,
-            self.sites.alt_seq,
-            self.sites["Corrected.AR"],
+        return (ref_matches, alt_matches)
+
+    def get_best_scores(self, fpr=0.05) -> pd.DataFrame:
+        """
+        Get the best motif score for each sequence, at a given false positive rate.
+        """
+        ref_matches, alt_matches = self._score_sequences(fpr=fpr)
+        scores = []
+
+        for ref_match, alt_match, (_, row) in zip(
+            ref_matches, alt_matches, self.sites.iterrows()
         ):
             for ref_match_l, alt_match_l, motif in zip(
-                ref_matches, alt_matches, self.motifs
+                ref_match, alt_match, self.motifs
             ):
-                ref_match, alt_match = None, None
-                if ref_match_l:
-                    ref_match = max(ref_match_l, key=lambda x: x[0])
-                if alt_match_l:
-                    alt_match = max(alt_match_l, key=lambda x: x[0])
+                # I'm avoiding any conditional logic on whether the max is in the
+                # ref or alt strand, this means we re-score both strands unnecesarily,
+                # but it makes the flow much simpler
+                combined_scores = ref_match_l + alt_match_l
+                if not combined_scores:
+                    continue
 
-                # FIXME: this can be cleaned up
-                if (ref_match is not None) or (alt_match is not None):
-                    scores["motif"].append(str(motif))
-                    scores["ar"].append(ar)
-                    scores["id"].append(rsid)
-                    if (alt_match is None) or (
-                        (ref_match is not None) and (ref_match[0] > alt_match[0])
-                    ):
-                        scores["ref_score"].append(ref_match[0])
-                        scores["pos"].append(ref_match[1])
-                        scores["strand"].append(ref_match[2])
-                        scores["alt_score"].append(
-                            ScoreSet.re_score(
-                                alt_seq, ref_match[1], ref_match[2], motif
-                            )
-                        )
+                (_, pos, strand) = max(combined_scores, key=lambda x: x[0])
 
-                    else:
-                        scores["alt_score"].append(alt_match[0])
-                        scores["pos"].append(alt_match[1])
-                        scores["strand"].append(alt_match[2])
-                        scores["ref_score"].append(
-                            ScoreSet.re_score(
-                                ref_seq, alt_match[1], alt_match[2], motif
-                            )
-                        )
-        score_set = pd.DataFrame.from_dict(scores)
-        score_set["score_diff"] = score_set.ref_score - score_set.alt_score
+                score = row.copy()
+                score["motif"] = str(motif)
+                score["motif_pos"] = pos
+                score["motif_strand"] = strand
+                score["ref_score"] = ScoreSet.re_score(
+                    score.ref_seq, pos, strand, motif
+                )
+                score["alt_score"] = ScoreSet.re_score(
+                    score.alt_seq, pos, strand, motif
+                )
+                score["score_diff"] = score.ref_score - score.alt_score
+
+                scores.append(score)
+
+        score_set = pd.DataFrame(scores)
         return score_set
 
     @staticmethod
